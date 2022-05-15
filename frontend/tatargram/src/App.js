3de042ebe9966b1f8c9fdd1b4d7d7fec6b658d38
Shakhzod Niyozov <shakhzodniyozov@yandex.ru>
2022-05-15 14:32:19 +0300

    Project structure changed

--- frontend/tatargram/src/App.js
+++ frontend/tatargram/src/App.js
@@ -1,16 +1,29 @@
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Button } from 'antd';
 
-ReactDOM.render(
-  <>
-    <Button type="primary">Primary Button</Button>
-    <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
-    <br />
-    <Button type="text">Text Button</Button>
-    <Button type="link">Link Button</Button>
-  </>,
-  document.getElementById('container'),
-);
+import './App.css';
+import { useState, useEffect } from "react";
+//import "./post.css"
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import { Routes, Route, Link } from 'react-router-dom';
+//import TodoApp from './Todo';
+
+function App() {
+
+    return (
+        <>
+           
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Register />} />
+            </Routes>
+
+
+
+
+        </>
+    );
+}
+
+export default App;

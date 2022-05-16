3a9a560989ae7d9d22a16753e8bc97408f48bc9a
Shakhzod Niyozov <shakhzodniyozov@yandex.ru>
2022-05-16 14:49:50 +0300

    Axios configured correctly.

--- frontend/tatargram/src/axios/axios.js
+++ frontend/tatargram/src/axios/axios.js
@@ -1,26 +1,29 @@
 import axios from "axios";
 
-axios.interceptors.response.use(response => {
+const ax = axios.create({
+    baseURL: `https://localhost:5001/api`,
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+});
+
+ax.interceptors.response.use(response => {
     return response;
 }, error => {
+    console.log("error", error)
     if (error.response.status === 401)
         window.location.href.replace("/signin");
     return Promise.reject(error);
 });
 
-axios.interceptors.request.use(request => {
+ax.interceptors.request.use(request => {
     const user = JSON.parse(localStorage.getItem("user"));
     if (user) {
         request.headers["Authorization"] = "Bearer " + user.accessToken;
     }
 
     return request;
-})
-
-export default axios.create({
-    baseURL: "https://localhost:5001/api",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
 });
+
+export default ax;

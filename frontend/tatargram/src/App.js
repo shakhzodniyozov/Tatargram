7b2f4c5741e2f0c55b12b8a946809437d58aafcb
TwoPacalypse <70624283+TwoPacalypse@users.noreply.github.com>
2022-05-16 14:28:57 +0300

    Added new route

--- frontend/tatargram/src/App.js
+++ frontend/tatargram/src/App.js
@@ -1,15 +1,15 @@
-
 import './css/App.css';
 import Home from './pages/Home';
 import { SignUp } from './pages/SignUp';
 import { Routes, Route, Link } from 'react-router-dom';
 import { SignIn } from './pages/SignIn';
-
+import Profile from './pages/Profile';
 function App() {
 
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Profile />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
         </Routes>
